1883c02c9172dcf95b285f161f71af3cbeedc4ea
Andreasjj <andreasjj@gmail.com>
2023-03-24 17:07:27 +0100

    make first test work

--- application/backend/tests/api/test_events.py
+++ application/backend/tests/api/test_events.py
@@ -1,8 +1,12 @@
 import pytest
 from flask import url_for
+from flask_jwt_extended import create_access_token
 
 
 @pytest.mark.usefixtures('client_class')
 class TestEventsSuit:
-    def test_events_get(self):
-        assert self.client.get(url_for('api.events.Events', method='get')).status_code == 401
+    def test_events_get(self, user):
+        token = create_access_token(identity=user)
+        headers = {"Authorization": f"Bearer {token}"}
+        response = self.client.get(url_for('api.events.Events', method='get'), headers=headers)
+        assert response.status_code == 200
--- application/backend/tests/conftest.py
+++ application/backend/tests/conftest.py
@@ -1,23 +1,49 @@
 import pytest
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from flask_migrate import Migrate, upgrade
 from pytest_postgresql import factories
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from app.models.user import User
+from app.models.slack_organization import SlackOrganization
 
-postgresql = factories.postgresql_proc()
+database_name = "pizza"
+postgresql = factories.postgresql_proc(dbname=database_name)
 
-@pytest.fixture(scope='session')
+
+@pytest.fixture
 def postgresql_url(postgresql):
-    return f'postgresql://{postgresql.user}:{postgresql.password}@{postgresql.host}:{postgresql.port}/{postgresql.dbname}'
+    connection = f'postgresql://{postgresql.user}:{postgresql.password}@{postgresql.host}:{postgresql.port}/{postgresql.dbname}'
+
+    conn = psycopg2.connect(
+        dbname='postgres',
+        user='postgres',
+        host=postgresql.host,
+        port=postgresql.port,
+        password=postgresql.password
+    )
+
+    # Create the "pizza" database
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cur = conn.cursor()
+    cur.execute('CREATE DATABASE %s;' % database_name)
+    cur.close()
+    conn.close()
+
+    return connection
+
 
 @pytest.fixture
 def environment_variables(postgresql_url, monkeypatch):
     monkeypatch.setenv('FLASK_ENV', 'test')
     monkeypatch.setenv('MQ_RPC_KEY', 'dontCare')
     monkeypatch.setenv('DATABASE_URL', postgresql_url)
+    monkeypatch.setenv('SECRET_KEY', 'verySuperSecretKey')
+
 
 @pytest.fixture
 def app(mocker, environment_variables):
-    mocker.patch('app.broker')
+    mocker.patch('app.application.broker')
     config = {
         "base": "app.config.Base",
         "test": "app.config.Test",
@@ -25,18 +51,41 @@ def app(mocker, environment_variables):
     }
     # Needs to be imported here as we need to set the environment variables in the environment_variables fixture
     # before we import it
-    from app import create_app
+    from app.application import create_app
     app = create_app(config)
     return app
+
 
 @pytest.fixture
 def db(app):
     db = SQLAlchemy(app=app)
-    db.create_all()
-    yield db
-    db.drop_all()
+    return db
 
-@pytest.fixture(scope='session')
+
+@pytest.fixture(autouse=True)
 def migrate(app, db):
     migrate = Migrate(app, db)
+    upgrade('../migrations')
     return migrate
+
+
+@pytest.fixture
+def slack_organization(app, db, migrate):
+    slack_organization = SlackOrganization(team_id="testSlackOrganizationId")
+    db.session.add(slack_organization)
+    db.session.commit()
+    return slack_organization
+
+
+@pytest.fixture
+def user(db, slack_organization):
+    user = User(
+        id="testUserId",
+        email="dont@care.invalid",
+        name="dontCare",
+        picture="doesntExist",
+        slack_organization_id=slack_organization.team_id
+    )
+    db.session.add(user)
+    db.session.commit()
+    return user
